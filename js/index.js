b0b72aecde855326655137e928fc2f10ad02695d
Hritik Pathak <hritik1511@gmail.com>
2022-07-26 21:35:46 +0530

    Initial commit

--- js/index.js
+++ js/index.js
@@ -22,3 +22,47 @@ btnCloseMenu.addEventListener("click", function () {
   video.classList.add("filter");
   opacity.classList.remove("opacity");
 });
+
+///////////////////////
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+prevBtn.addEventListener("click", function () {
+  prevBtn.classList.add("backGround");
+  nextBtn.classList.remove("backGround");
+});
+nextBtn.addEventListener("click", function () {
+  prevBtn.classList.remove("backGround");
+  nextBtn.classList.add("backGround");
+});
+
+///////
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
